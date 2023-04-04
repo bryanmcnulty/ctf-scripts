32e47430f1ff06468ef2c900c903b8e25248cb87
Bryan McNulty <bryanmcnulty@protonmail.com>
2023-04-03 19:43:32 -0500

    Add ctfscan port scanner
    This is just a fancy wrapper for nmap

---
+++ ctfscan.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+[ -z "$1" ] && echo "$0 <IP>" && exit
+
+# UDP ports for DNS,TFTP,KRB5,NTP,SNMP,IPMI,IKE,ISAKMP
+udp_ports="53,69,88,123,161,162,500,623,4500,10161,10162"
+# All TCP ports
+tcp_ports="-"
+
+log_dir=$(mktemp -d)
+cd $log_dir
+
+echo "[+] Starting TCP scan"
+sudo nmap -v -Pn -sS -n -p $tcp_ports --min-rate=1000 -T4 $1 -oN ./tcp-discovery.log
+
+open_tcp_ports=$(grep '^[0-9]' ./tcp-discovery.log |
+	cut -d '/' -f 1 |
+	tr '\n' ',' |
+	sed 's/,$//')
+
+[ -n "$open_tcp_ports" ] && echo "[+] Found open TCP ports: $open_tcp_ports"
+echo "[+] Starting UDP scan"
+sudo nmap -v -Pn -sU -n -p $udp_ports --min-rate=50 -T4 $1 -oN ./udp-discovery.log
+
+open_udp_ports=$(grep '^[0-9].* *open +' ./udp-discovery.log |
+	cut -d '/' -f 1 |
+	tr '\n' ',' |
+	sed 's/,$//')
+
+[ -n "$open_udp_ports" ] && echo "[+] Found open UDP ports: $open_udp_ports"
+
+mkdir ./tcp ./udp
+[ -z $open_tcp_ports ] || sudo nmap $1 -sS -n -p "$open_tcp_ports" -sV -sC -oA ./tcp/scan
+[ -z $open_udp_ports ] || sudo nmap $1 -sU -n -p "$open_udp_ports" -sV -sC -oA ./udp/scan
+
+cd $OLDPWD
+mkdir -p ./logs
+
+dir="./logs/port-scan-$(date +%s)"
+mv $log_dir $dir
+sudo chown -R "$USER:$USER" $dir
+
+echo "[*] Done!"
